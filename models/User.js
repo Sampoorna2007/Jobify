61ed5975e1b2ea59df09391e27cab88918da5c49
Sampoorna Prabhu <sampoornaprabhu371@gmail.com>
2026-05-25 20:25:33 +0530

    Initial Commit:Working registration and MongoDB connection

---
+++ models/User.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+{
+    name: {
+        type: String,
+        required: true
+    },
+
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+
+    password: {
+        type: String,
+        required: true
+    },
+
+    role: {
+        type: String,
+        enum: ["admin", "employer", "jobseeker"],
+        default: "jobseeker"
+    },
+
+    skills: [{
+        type: String
+    }],
+
+    education: [{
+        type: String
+    }],
+
+    experience: [{
+        type: String
+    }],
+
+    resume: {
+        type: String
+    },
+
+    companyName: {
+        type: String
+    }
+},
+{
+    timestamps: true
+}
+);
+
+module.exports = mongoose.model("User", userSchema);
